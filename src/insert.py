658befa3b81c1f552a41d1eb67d1edb3b52f3f8b
zhaochengqiang <zhaochengqiang@yonyou.com>
2018-01-22 20:15:57 +0800

    add

---
+++ src/insert.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+ 
+import pymysql
+ 
+# 打开数据库连接
+db = pymysql.connect("127.0.0.1","root","root","zhaoTable" )
+ 
+# 使用cursor()方法获取操作游标 
+cursor = db.cursor()
+ 
+# SQL 插入语句
+# sql = """INSERT INTO EMPLOYEE(FIRST_NAME,
+#          LAST_NAME, AGE, SEX, INCOME)
+#          VALUES ('Mac', 'Mohan', 20, 'M', 2000)"""
+         
+sql = "INSERT INTO EMPLOYEE(FIRST_NAME, \
+        LAST_NAME, AGE, SEX, INCOME) \
+        VALUES ('%s', '%s', '%d', '%c', '%d' )" % \
+        ('Mac10', 'Mohan1', 201, 'A', 20001)
+try:
+   # 执行sql语句
+   cursor.execute(sql)
+   # 提交到数据库执行
+   db.commit()
+except:
+   # 如果发生错误则回滚
+   db.rollback()
+ 
+# 关闭数据库连接
+db.close()
